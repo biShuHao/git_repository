c58d407227980dfe174b1b20ec9b9aa6a5aea80c
TheGrandTour <58095749+biShuHao@users.noreply.github.com>
2020-05-13 15:21:25 +0800

    Update Test1.java

--- git_test1/src/main/java/com/usian/java/Test1.java
+++ git_test1/src/main/java/com/usian/java/Test1.java
@@ -2,6 +2,7 @@ package com.usian.java;
 
 public class Test1 {
     //修改了Test1
+    //这是远程github仓库中的代码
     public static void main(String[] args) {
         //github端的修改
         System.out.println("师姐你好");
